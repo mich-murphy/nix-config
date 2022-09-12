5e74f57109e8c0de0f7ce2bb54713e180621a972
mich-murphy <mich+git@elmurphy.com>
2022-09-12 13:38:34 +1000

    updated yabai and skhd configs

--- hosts/laptop/darwin-configuration.nix
+++ hosts/laptop/darwin-configuration.nix
@@ -1,11 +1,13 @@
 { config, pkgs, user, host, ... }:
 
-let yabai = pkgs.yabai.overrideAttrs (old: rec {
-  src = builtins.fetchTarball {
-    url = https://github.com/koekeishiya/yabai/releases/download/v4.0.2/yabai-v4.0.2.tar.gz;
-    sha256 = "00nxzk1g0hd8jqd1r0aig6wdsbpk60551qxnvvqb9475i8qbzjf6";
-  };
-}); in
+let 
+  yabai = pkgs.yabai.overrideAttrs (old: rec {
+    src = builtins.fetchTarball {
+      url = https://github.com/koekeishiya/yabai/releases/download/v4.0.2/yabai-v4.0.2.tar.gz;
+      sha256 = "00nxzk1g0hd8jqd1r0aig6wdsbpk60551qxnvvqb9475i8qbzjf6";
+    };
+  }); 
+in
 {
   nixpkgs.config.allowUnfree = true;
   time.timeZone = "Australia/Melbourne";
@@ -127,6 +129,7 @@ let yabai = pkgs.yabai.overrideAttrs (old: rec {
       "alacritty"
       "spaceid"
       "1password"
+      "setapp"
     ];
   };
 
@@ -152,8 +155,11 @@ let yabai = pkgs.yabai.overrideAttrs (old: rec {
       enableScriptingAddition = true;
       package = yabai;
       config = {
-        focus_follows_mouse = "autofocus";
+        focus_follows_mouse = "off";
 	mouse_follows_focus = "off";
+	mouse_modifier = "fn";
+	mouse_action1 = "move";
+	mouse_action2 = "resize";
         layout = "bsp";
         split_ratio = 0.5;
         auto_balance = "off";
@@ -161,15 +167,22 @@ let yabai = pkgs.yabai.overrideAttrs (old: rec {
         bottom_padding = 5;
         left_padding = 5;
         right_padding = 5;
+	window_shadow = "float";
         window_gap = 5;
         window_placement = "second_child";
         extraConfig = ''
           yabai -m rule --add title='Preferences' manage=off layer=above
           yabai -m rule --add title='^(Opening)' manage=off layer=above
           yabai -m rule --add title='Library' manage=off layer=above
+          yabai -m rule --add app='^Calculator$' manage=off layer=above
+          yabai -m rule --add app='^App Store$' manage=off layer=above
           yabai -m rule --add app='^System Preferences$' manage=off layer=above
-          yabai -m rule --add app='Activity Monitor' manage=off layer=above
-          # yabai -m rule --add app='Finder' manage=off layer=above
+          yabai -m rule --add app='^Activity Monitor$' manage=off layer=above
+	  yabai -m rule --add app='Setapp' manage=off layer=above
+	  yabai -m rule --add app='1Password' manage=off layer=above
+	  yabai -m rule --add app='Bartender' manage=off layer=above
+	  yabai -m rule --add app='CleanMyMac' manage=off layer=above
+	  yabai -m rule --add app='Dash' manage=off layer=above
           yabai -m rule --add app='^System Information$' manage=off layer=above
         '';
       };
@@ -181,10 +194,14 @@ let yabai = pkgs.yabai.overrideAttrs (old: rec {
         # Applications Shortcuts
         cmd - return : /Applications/Alacritty.App/Contents/MacOS/alacritty
 	cmd + shift - return : /Applications/Firefox.App/Contents/MacOS/firefox
+	cmd + shift - f : /Applications/Alacritty.App/Contents/MacOS/alacritty -e ranger
         # Toggle Window
-        lalt - t : yabai -m window --toggle float && yabai -m window --grid 4:4:1:1:2:2
+        lalt - t : yabai -m window --toggle float; yabai -m window --grid 4:4:1:1:2:2
         lalt - f : yabai -m window --toggle zoom-fullscreen
+	lalt + shift - f : yabai -m window --toggle native-fullscreen
         lalt - q : yabai -m window --close
+	# Toggle Gaps
+	lalt - g : yabai -m space --toggle padding; yabai -m space --toggle gap
         # Focus Window
         lalt - k : yabai -m window --focus north
         lalt - j : yabai -m window --focus south
@@ -196,16 +213,18 @@ let yabai = pkgs.yabai.overrideAttrs (old: rec {
         shift + lalt - h : yabai -m window --swap west
         shift + lalt - l : yabai -m window --swap east
         # Resize Window
-        lalt + cmd - h : yabai -m window --resize left:-50:0 && yabai -m window --resize right:-50:0
-        lalt + cmd - l : yabai -m window --resize left:50:0 && yabai -m window --resize right:50:0
-        lalt + cmd - k : yabai -m window --resize up:-50:0 && yabai -m window --resize down:-50:0
-        lalt + cmd - j : yabai -m window --resize up:-50:0 && yabai -m window --resize down:-50:0
+        lalt + cmd - h : yabai -m window --resize left:-50:0; yabai -m window --resize right:-50:0
+        lalt + cmd - l : yabai -m window --resize left:50:0; yabai -m window --resize right:50:0
+        lalt + cmd - k : yabai -m window --resize bottom:0:-50; yabai -m window --resize top:0:-50
+        lalt + cmd - j : yabai -m window --resize bottom:0:50; yabai -m window --resize top:0:50
+	# Balance All Windows
+	lalt + cmd - e : yabai -m space --balance
         # Send to Space
-        shift + lalt - 1 : yabai -m window --space 1
-        shift + lalt - 2 : yabai -m window --space 2
-        shift + lalt - 3 : yabai -m window --space 3
-        shift + lalt - 4 : yabai -m window --space 4
-        shift + lalt - 5 : yabai -m window --space 5
+        shift + lctrl - 1 : yabai -m window --space 1; yabai -m space --focus 1
+        shift + lctrl - 2 : yabai -m window --space 2; yabai -m space --focus 2
+        shift + lctrl - 3 : yabai -m window --space 3; yabai -m space --focus 3
+        shift + lctrl - 4 : yabai -m window --space 4; yabai -m space --focus 4
+        shift + lctrl - 5 : yabai -m window --space 5; yabai -m space --focus 5
       '';
     };
   };
