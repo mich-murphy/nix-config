385e48544b4e4c57a10013bf1288e94c308e4df5
mich-murphy <mich+git@elmurphy.com>
2022-09-13 21:37:58 +1000

    moved part of treesitter config to treesitter-text-objects

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -332,35 +332,6 @@
                   node_decremental = '<c-backspace>',
                 },
               },
-              move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                  [']m'] = '@function.outer',
-                  [']]'] = '@class.outer',
-                },
-                goto_next_end = {
-                  [']M'] = '@function.outer',
-                  [']['] = '@class.outer',
-                },
-                goto_previous_start = {
-                  ['[m'] = '@function.outer',
-                  ['[['] = '@class.outer',
-                },
-                goto_previous_end = {
-                  ['[M'] = '@function.outer',
-                  ['[]'] = '@class.outer',
-                },
-              },
-              swap = {
-                enable = true,
-                swap_next = {
-                  ['<leader>a'] = '@parameter.inner',
-                },
-                swap_previous = {
-                  ['<leader>A'] = '@parameter.inner',
-                },
-              },
             }
             EOF
           '';
@@ -382,6 +353,35 @@
                     ["if"] = "@function.inner",
                     ["ac"] = "@class.outer",
                     ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                  },
+                },
+                move = {
+                  enable = true,
+                  set_jumps = true, -- whether to set jumps in the jumplist
+                  goto_next_start = {
+                    [']m'] = '@function.outer',
+                    [']]'] = '@class.outer',
+                  },
+                  goto_next_end = {
+                    [']M'] = '@function.outer',
+                    [']['] = '@class.outer',
+                  },
+                  goto_previous_start = {
+                    ['[m'] = '@function.outer',
+                    ['[['] = '@class.outer',
+                  },
+                  goto_previous_end = {
+                    ['[M'] = '@function.outer',
+                    ['[]'] = '@class.outer',
+                  },
+                },
+                swap = {
+                  enable = true,
+                  swap_next = {
+                    ['<leader>a'] = '@parameter.inner',
+                  },
+                  swap_previous = {
+                    ['<leader>A'] = '@parameter.inner',
                   },
                 },
               },
