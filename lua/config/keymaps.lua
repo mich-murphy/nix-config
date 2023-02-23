266af46257f9784631d1a5b7a0069c5fe57a2910
mich-murphy <github@elmurphy.com>
2023-02-23 15:48:41 +1100

    [keymaps] adding keymaps and moving undo keymap to plugin config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,16 @@ vim.keymap.set("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window 
 vim.keymap.set("n", "<A-Left>", "<cmd>vertical resize +2<cr>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<A-Right>", "<cmd>vertical resize -2<cr>", { desc = "Increase window width" })
 
+-- Move lines in visual mode with autoindent
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Maintain cursor position during movements
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 -- Better paste
 vim.keymap.set("v", "p", '"_dP', { desc = "Paste without changing registry"})
-
-vim.keymap.set("n", "<leader>su", "<cmd>MundoToggle<cr>", { desc = "Search undo tree" })
--- lua/plugins/undo.lua
+++ lua/plugins/undo.lua
@@ -1,4 +1,7 @@
 return {
 
   { "simnalamburt/vim-mundo" },
+
+  -- Set keymap to toggle plugin
+  vim.keymap.set("n", "<leader>su", "<cmd>MundoToggle<cr>", { desc = "Search undo tree" })
 }
