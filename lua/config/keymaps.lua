7f497b8901846bfb560422df113557a89e8d367b
mich-murphy <github@elmurphy.com>
2023-02-23 15:26:53 +1100

    [options][keymaps] adding personalised options and keymaps as well as vim mundo

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Keyvim.keymap.sets are automatically loaded on the VeryLazy event
+-- Default keyvim.keymap.sets that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keyvim.keymap.sets here
+
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<A-Left>", "<cmd>vertical resize +2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<A-Right>", "<cmd>vertical resize -2<cr>", { desc = "Increase window width" })
+
+-- Better paste
+vim.keymap.set("v", "p", '"_dP', { desc = "Paste without changing registry"})
+
+vim.keymap.set("n", "<leader>su", "<cmd>MundoToggle<cr>", { desc = "Search undo tree" })
