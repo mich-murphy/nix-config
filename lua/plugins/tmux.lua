1cb704f4eaf9469d4082a86b9bb6dbd7692d323f
mich-murphy <github@elmurphy.com>
2023-02-27 16:01:21 +1100

    [tmux] adding config for vim tmux navigator

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,9 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate to left tmux pane" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate to below tmux pane" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate to above tmux pane" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate to right tmux pane" },
+  }
+}
