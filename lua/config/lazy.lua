f0e1e5ceadf937c48e4bd35b1af5de4dd1742500
mich-murphy <github@elmurphy.com>
2023-04-29 22:55:40 +1000

    [treesiter] resolving overwrite of parser imports

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
@@ -37,7 +36,7 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,3 @@
 return {
-
-  -- add context and previous disabling of indentation for Python
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-context",
-    },
-    opts = {
-      indent = { enable = true },
-    }
-  },
+  "nvim-treesitter/nvim-treesitter-context",
 }
