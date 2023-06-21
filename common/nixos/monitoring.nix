8dfb0b2db7d1fffaca71fddb63b871dfe91b4634
mich-murphy <github@elmurphy.com>
2023-06-22 09:48:28 +1000

    [media] cleaning up comments from monitoring config

--- common/nixos/monitoring.nix
+++ common/nixos/monitoring.nix
@@ -36,12 +36,6 @@ in
             http_addr = "127.0.0.1";
           };
         };
-        # provision = {
-        #   enable = true;
-        #   datasources.settings = {
-        #     datasources.prometheus.url = "http://127.0.0.1:${toString cfg.prometheus-port}";
-        #   };
-        # };
       };
       prometheus = {
         enable = true;
