a8210908844fec24cdc85098893cecf9388ecb55
mm <mich+git@elmurphy.com>
2023-09-03 08:56:59 +1000

    reducing borgbackup directories

--- common/nixos/borgbackup.nix
+++ common/nixos/borgbackup.nix
@@ -15,12 +15,7 @@ in
       "media" = {
         paths = [
           "/data/media/music"
-          "/data/media/books"
-          "/data/media/comics"
-          "/data/media/manga"
-          "/data/media/audiobooks"
           "/data/backups/RoonBackups"
-          "/var/lib/audiobookshelf/audiobooks"
         ];
         repo = "g268tdfo@g268tdfo.repo.borgbase.com:repo";
         encryption = {
