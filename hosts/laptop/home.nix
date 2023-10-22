5f7412ef5ea18bd39892f2ce6d8029826a2a510b
mich-murphy <github@elmurphy.com>
2023-10-23 09:40:59 +1100

    [karabiner] adding key remaps to darwin conf dir

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -32,24 +32,31 @@
     yazi.enable = true;
   };
 
-  xdg.configFile."spotifyd/spotifyd.conf" = {
-    enable = true;
-    target = "spotifyd/spotifyd.conf";
-    text = ''
-      [global]
-      username = "spotify@elmurphy.com"
-      password_cmd = "op read op://Private/spotify/password"
-      backend = "portaudio"
-      device_name = "macbook"
-      device_type = "computer"
-      no_audio_cache = true
-      bitrate = 320
-      volume_normalisation = true
-      normalisation_pregain = -10
-      autoplay = true
-      volume_controller = "softvol"
-      zeroconf_port = 1234
-    '';
+  xdg.configFile = {
+    "karabiner" = {
+      enable = true;
+      recursive = true;
+      source = ./karabiner;
+    };
+    "spotifyd/spotifyd.conf" = {
+      enable = true;
+      target = "spotifyd/spotifyd.conf";
+      text = ''
+        [global]
+        username = "spotify@elmurphy.com"
+        password_cmd = "op read op://Private/spotify/password"
+        backend = "portaudio"
+        device_name = "macbook"
+        device_type = "computer"
+        no_audio_cache = true
+        bitrate = 320
+        volume_normalisation = true
+        normalisation_pregain = -10
+        autoplay = true
+        volume_controller = "softvol"
+        zeroconf_port = 1234
+      '';
+    };
   };
 
   programs.home-manager.enable = true;
