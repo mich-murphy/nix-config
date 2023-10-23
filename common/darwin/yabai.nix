fa85455e99291c20c6c0ad448769606c733163c6
mich-murphy <github@elmurphy.com>
2023-10-23 20:47:34 +1100

    [yabai] reverting kitty launch path

--- common/darwin/yabai.nix
+++ common/darwin/yabai.nix
@@ -80,7 +80,7 @@ in
           resize < e : yabai -m space --balance
 
           # Applications Shortcuts
-          alt - return : /Applications/kitty.app/Contents/MacOS/kitty --single-instance -d ~/dev
+          alt - return : /Applications/kitty.app/Contents/MacOS/kitty --single-instance -d ~
           shift + alt - return : /Applications/Firefox.App/Contents/MacOS/firefox
           # Toggle Window
           alt - t : yabai -m window --toggle float; yabai -m window --grid 4:4:1:1:2:2
