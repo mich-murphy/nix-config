b2f84e73a574a32745336b02da0b21db59d49180
mich-murphy <github@elmurphy.com>
2024-10-04 17:43:36 +1000

    updating wezterm to manage pane navigation with nvim

--- home/modules/wezterm.nix
+++ home/modules/wezterm.nix
@@ -28,93 +28,36 @@ in {
       enableZshIntegration = false;
       package = fakepkg "wezterm";
       extraConfig = ''
-        local config = {}
+        local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+        local config = wezterm.config_builder()
+
+        -- Show which key table is active in the status area
+        wezterm.on("update-right-status", function(window, pane)
+          local name = window:active_key_table()
+          if name then
+            name = "TABLE: " .. name
+          end
+          window:set_right_status(name or "")
+        end)
 
         config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
         config.keys = {
+          { key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
+          { key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
+          { key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
+          { key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+          { key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+          { key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({confirm = true }) },
+          { key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+          { key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+          { key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+          { key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+          { key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+          { key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+          { key = "w", mods = "LEADER", action = wezterm.action.ShowTabNavigator },
           {
-            mods = "LEADER",
-            key = "c",
-            action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-          },
-          {
-            mods = "LEADER",
-            key = "x",
-            action = wezterm.action.CloseCurrentPane({confirm = true }),
-          },
-          {
-            mods = "LEADER",
-            key = "p",
-            action = wezterm.action.ActivateTabRelative(-1),
-          },
-          {
-            mods = "LEADER",
-            key = "n",
-            action = wezterm.action.ActivateTabRelative(1),
-          },
-          {
-            mods = "LEADER",
-            key = "|",
-            action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-          },
-          {
-            mods = "LEADER",
-            key = "-",
-            action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-          },
-          {
-            mods = "CTRL",
-            key = "h",
-            action = wezterm.action.ActivatePaneDirection("Left"),
-          },
-          {
-            mods = "CTRL",
-            key = "j",
-            action = wezterm.action.ActivatePaneDirection("Down"),
-          },
-          {
-            mods = "CTRL",
-            key = "k",
-            action = wezterm.action.ActivatePaneDirection("Up"),
-          },
-          {
-            mods = "CTRL",
-            key = "l",
-            action = wezterm.action.ActivatePaneDirection("Right"),
-          },
-          {
-            mods = "CTRL",
-            key = "LeftArrow",
-            action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-          },
-          {
-            mods = "CTRL",
-            key = "RightArrow",
-            action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-          },
-          {
-            mods = "CTRL",
-            key = "DownArrow",
-            action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-          },
-          {
-            mods = "CTRL",
-            key = "UpArrow",
-            action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-          },
-          {
-            mods = "LEADER",
-            key = "z",
-            action = wezterm.action.TogglePaneZoomState,
-          },
-          {
-            mods = "LEADER",
-            key = "[",
-            action = wezterm.action.ActivateCopyMode,
-          },
-          {
-            mods = "LEADER",
             key = ",",
+            mods = "LEADER",
             action = wezterm.action.PromptInputLine({
               description = "Enter name for tab",
               action = wezterm.action_callback(function(window, pane, line)
@@ -125,25 +68,40 @@ in {
             }),
           },
           {
+            key = "r",
             mods = "LEADER",
-            key = "w",
-            action = wezterm.action.ShowTabNavigator,
+            action = wezterm.action.ActivateKeyTable {
+              name = "resize_pane",
+              one_shot = false,
+            },
+          },
+        }
+
+        config.key_tables = {
+          -- Defines the keys that are active in our resize-pane mode.
+          resize_pane = {
+            { key = "h", action = wezterm.action.AdjustPaneSize { "Left", 1 } },
+            { key = "l", action = wezterm.action.AdjustPaneSize { "Right", 1 } },
+            { key = "k", action = wezterm.action.AdjustPaneSize { "Up", 1 } },
+            { key = "j", action = wezterm.action.AdjustPaneSize { "Down", 1 } },
+            -- Cancel the mode by pressing escape
+            { key = "Escape", action = "PopKeyTable" },
           },
         }
 
         -- Leader + number to switch tab
-        for i = 0, 9 do
+        for i = 1, 8 do
           table.insert(config.keys, {
-            mods = "LEADER",
             key = tostring(i),
-            action = wezterm.action.ActivateTab(i),
+            mods = "LEADER",
+            action = wezterm.action.ActivateTab(i - 1),
           })
         end
 
         -- Tab bar
         config.hide_tab_bar_if_only_one_tab = false
         config.use_fancy_tab_bar = false
-        config.tab_and_split_indices_are_zero_based = true
+        config.switch_to_last_active_tab_when_closing_tab = true
 
         -- General
         config.color_scheme = 'tokyonight_night'
@@ -153,6 +111,13 @@ in {
         config.window_decorations = "RESIZE"
         config.adjust_window_size_when_changing_font_size = false
         config.window_close_confirmation = 'NeverPrompt'
+
+        smart_splits.apply_to_config(config, {
+          direction_keys = { "h", "j", "k", "l" },
+          modifiers = {
+            move = "CTRL",
+          },
+        })
 
         return config;
       '';
