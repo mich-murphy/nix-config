7a0c7e8c908a3c4214467bd65ef6c84c3110d49b
mich-murphy <github@elmurphy.com>
2026-02-26 14:16:46 +1100

    fix(secrets): updating secrets to reflect hosts that have been removed

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,7 @@
 let
   media = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVyN0R5mTtfcbkmVXjicuvSRotJY4IOuetOgPyG2lg8";
-  services = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOt6E5Phwiv2S2yed58vOBzsyeQJ/ZhiBDAA8j+gGyq7";
-  downloads = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGz3BlJy2XlBL2M+oDzETkN/8nvH4y382HOlRP6/y7ZW";
-  servers = [media services downloads];
 in {
-  "userPass.age".publicKeys = servers;
+  "userPass.age".publicKeys = [media];
   "nextcloudPass.age".publicKeys = [media];
   "acmeCredentials.age".publicKeys = [media];
   "objectStorage.age".publicKeys = [media];
@@ -12,9 +9,8 @@ in {
   "nextcloudBorgPass.age".publicKeys = [media];
   "freshrssPass.age".publicKeys = [media];
   "giteaDbPass.age".publicKeys = [media];
-  "sambaPass.age".publicKeys = servers;
+  "sambaPass.age".publicKeys = [media];
   "murmurPass.age".publicKeys = [media];
-  "delugePass.age".publicKeys = [services];
   "matrixSharedSecret.age".publicKeys = [media];
   "immichBorgPass.age".publicKeys = [media];
   "gitlabPass.age".publicKeys = [media];
@@ -23,8 +19,8 @@ in {
   "gitlabJwsFile.age".publicKeys = [media];
   "gitlabOtpFile.age".publicKeys = [media];
   "gitlabSecretFile.age".publicKeys = [media];
-  "netdataClaimToken.age".publicKeys = servers;
+  "netdataClaimToken.age".publicKeys = [media];
   "gitBorgPass.age".publicKeys = [media];
-  "tailscaleAuthKey.age".publicKeys = servers;
+  "tailscaleAuthKey.age".publicKeys = [media];
   "paperlessPass.age".publicKeys = [media];
 }
